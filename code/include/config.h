ffc14a586d5046c3e1d015002f138712c1f36d23
Hung Nguyen <hungng@seas.upenn.edu>
2016-04-24 20:13:01 -0400

    back to 100

--- code/include/config.h
+++ code/include/config.h
@@ -30,7 +30,7 @@
 
 #define MAX_CLIENT_BUFFER_LENGTH    256     // Maximum client list encoded length
 
-#define UDP_RECEIVER_QUEUE_SIZE     1000    // Number of payload receiving kept track by receiver to prevent duplicate
+#define UDP_RECEIVER_QUEUE_SIZE     100     // Number of payload receiving kept track by receiver to prevent duplicate
 #define NUM_UDP_RETRIES             2       // Default number of UDP resend before notify crash
 #define UDP_TIMEOUT                 2000    // Timeout before trying resend UDP payload in miliseconds
 #define ACK_MONITOR_INTERVAL        1       // Ack check interval in seconds
